dca06815fc8b7b862b7882ea4b7eeaa08206d98f
Rahul Macha <rahul.macha567@gmail.com>
2023-03-09 12:03:23 +0530

    env changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  mapbox:{
+    accessToken:"pk.eyJ1IjoicmFodWxtYWNoYSIsImEiOiJja21sa3d5Ym8xYm9tMm5vNGZmZzg4emppIn0.9H0Dg451GSADHWboKqHE2g"
+  }
 };
